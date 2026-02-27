b0f11b12c80361734a9bd58957d3f6ea0a76e919
Elon Aseneka <elonsantos63@gmail.com>
2026-02-27 13:19:10 +0300

    fix actions - handle k3s idempotency

--- k3s/configs.sh
+++ k3s/configs.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-kubectl create namespace techietenka
+set -e
+
+# Namespace
+kubectl create namespace techietenka --dry-run=client -o yaml | kubectl apply -f -
 kubectl config set-context --current --namespace=techietenka
-kubectl create secret generic techietenka-env-vars --from-env-file=prod.env
-kubectl create configmap mosquitto-config --from-file=mosquitto/config/mosquitto.conf
-kubectl create secret generic mosquitto-pwfile --from-file=mosquitto/config/pwfile
+
+# Secrets & ConfigMaps
+kubectl create secret generic techietenka-env-vars \
+  --from-env-file=prod.env \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+kubectl create configmap mosquitto-config \
+  --from-file=mosquitto/config/mosquitto.conf \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+kubectl create secret generic mosquitto-pwfile \
+  --from-file=mosquitto/config/pwfile \
+  --dry-run=client -o yaml | kubectl apply -f -
