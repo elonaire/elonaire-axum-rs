a9850b57c0d2781414b7699b6abe7065c04ae13e
Elon Aseneka <elonsantos63@gmail.com>
2026-09-07 07:59:18 +0300

    Configure ArgoCD GUI

--- k3s/configs.sh
+++ k3s/configs.sh
@@ -3,7 +3,7 @@ set -e
 
 # ArgoCD
 kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
-kubectl apply -n argocd --server-side -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch deployment argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]'
 
 # Namespace
