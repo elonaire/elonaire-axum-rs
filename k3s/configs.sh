7a9905486e83f293f637e1b080b31b53d2fbc944
Elon Aseneka <elonsantos63@gmail.com>
2026-09-07 07:49:25 +0300

    Configure ArgoCD GUI

--- k3s/configs.sh
+++ k3s/configs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
+# ArgoCD
+kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch deployment argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]'
+
 # Namespace
 kubectl create namespace techietenka --dry-run=client -o yaml | kubectl apply -f -
 kubectl config set-context --current --namespace=techietenka
-
-# ArgoCD
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch deployment argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]'
 
 # Secrets & ConfigMaps
 kubectl create secret generic techietenka-env-vars \
